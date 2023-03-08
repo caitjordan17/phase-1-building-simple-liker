98f66e40da459a935e4167631ac9ab072da36545
Caitlin Jordan <jordan.cait17@gmail.com>
2023-03-07 21:31:48 -0800

    completed lab

--- main.js
+++ main.js
@@ -4,6 +4,52 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener('DOMContentLoaded',() => {
+  console.log('Dom content loaded')
+  const errorMessage = document.querySelector('#modal')
+  errorMessage.classList.add('hidden')
+  //findLikes()
+  clickListener()
+})
+//document.getElementsByClassName("like-glyph").addEventListener("click", mimicServerCall);
+
+function clickListener(){
+  document.addEventListener('click', (event)=>{
+    //if i click on heart, console log "like"
+    //otherwise do nothing
+    if(event.target.classList[0] === 'like-glyph'){
+      mimicServerCall()
+        .then((resp) => {
+          const activated = event.target.classList.contains('activated-heart');
+          if(activated){
+            event.target.classList.remove('activated-heart')
+            event.target.innerHTML = EMPTY_HEART;
+          } else {
+            event.target.classList.add('activated-heart')
+            event.target.innerHTML = FULL_HEART;
+          }
+          activated
+        })
+        .catch((error) => {
+          document.querySelector('#modal').classList.remove('hidden');
+          setTimeout(() => document.querySelector('#modal').classList.add('hidden'), 3000);
+        });
+    }
+    //console.log(event.target)
+    });
+  }
+
+// function findLikes(){
+//   const likeArr = document.querySelectorAll(".like-glyph")
+  
+//   likeArr.forEach((singularLike)=> {
+//     singularLike.addEventListener('click', ()=> console.log("you found me"))
+//   })
+// }
+
+
+
+
 
 
 
